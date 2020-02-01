7e1afbcc91fb69125708564c52f5bc8a262257f0
Robert Kramer <robert.n.kramer@gmail.com>
2020-01-31 17:48:04 -0800

    refactoring code

--- script.js
+++ script.js
@@ -1,48 +1,17 @@
 (function() {
   const addButton = document.getElementById("add-todo");
-  const hideCompleted = document.getElementById("hide-complete");
-  const deleteAllCompleted = document.getElementById("remove-complete");
-  const todoList = document.getElementById("todo-list");
-  const todoInput = document.getElementById("todo-input");
+  addButton.addEventListener("click", newTodo);
+
+  document.addEventListener("keyup", e => {
+    if (e.key === "Enter") newTodo();
+  });
+
   let todosArray = JSON.parse(window.localStorage.getItem("todoList")) || [];
   let displayCompleted = true;
 
-  function init() {
-    addListeners();
-    renderTodos();
-    todoInput.focus();
-  }
-
-  function addListeners() {
-    document.addEventListener("keyup", enterKeyPress);
-    todoList.addEventListener("click", addButtonClick);
-    addButton.addEventListener("click", newTodo);
-    hideCompleted.addEventListener("click", hideCompletedAction);
-    deleteAllCompleted.addEventListener("click", deleteAllAction);
-  }
-
-  function enterKeyPress(e) {
-    if (e.key === "Enter") newTodo();
-  }
-
-  function addButtonClick(e) {
-    const element = e.srcElement.localName;
-    const itemId = e.target.parentNode.id;
-
-    if (element === "i") deleteTodo(itemId);
-    if (element === "span") toggleCompleted(itemId);
-  }
-
-  function hideCompletedAction() {
-    displayCompleted = !displayCompleted;
-    renderTodos();
-  }
-
-  function deleteAllAction() {
-    updateList(todosArray.filter(todo => !todo.completed));
-  }
-
   function newTodo() {
+    const todoInput = document.getElementById("todo-input");
+
     if (todoInput.value) {
       updateList([...todosArray, new todoObject(todoInput.value)]);
       todoInput.value = "";
@@ -58,68 +27,100 @@
     };
   }
 
+  function createTodoItem(todo) {
+    const liElem = createLiElement(todo.id);
+    liElem.appendChild(createSpanElement(todo));
+    liElem.appendChild(createIconElement());
+    return liElem;
+  }
+
+  function createLiElement(id) {
+    const liElem = document.createElement("li");
+    liElem.id = `${id}`;
+    liElem.className = "todo-item";
+    return liElem;
+  }
+
+  function createSpanElement({ id, text, completed }) {
+    const spanElem = document.createElement("span");
+    spanElem.innerText = `${text}`;
+    completed
+      ? spanElem.classList.add("completed")
+      : spanElem.classList.remove("completed");
+
+    spanElem.addEventListener("click", () => {
+      const todoIndex = locateTodoItem(id);
+      // const toggleList = todosArray.map(todo => {
+      //   return { ...todo };
+      // });
+
+      // if (todoIndex >= 0) {
+      //   toggleList[todoIndex].completed = !toggleList[todoIndex].completed;
+      // }
+      // updateList(toggleList);
+
+      console.log(todoIndex);
+    });
+
+    return spanElem;
+  }
+
+  function createIconElement() {
+    const iconElem = document.createElement("i");
+    iconElem.classList.add("fa", "fa-trash");
+    return iconElem;
+  }
+
   function renderTodos() {
+    const todoList = document.getElementById("todo-list");
     todoList.innerHTML = "";
+
     if (todosArray.length) {
       todosArray.forEach(todo => {
         if (displayCompleted) {
-          createTodoItem(todo);
-          hideCompleted.innerText = "Hide Completed";
+          todoList.appendChild(createTodoItem(todo));
+          // hideCompleted.innerText = "Hide Completed";
         } else if (!todo.completed) {
-          createTodoItem(todo);
-          hideCompleted.innerText = "Show Completed";
+          todoList.appendChild(createTodoItem(todo));
+          // hideCompleted.innerText = "Show Completed";
         }
       });
       if (!todoList.innerHTML) {
         todoList.innerHTML = "All todos are completed";
-        hideCompleted.innerText = "Show Completed";
+        // hideCompleted.innerText = "Show Completed";
       }
     } else {
       todoList.innerHTML = "No todos";
     }
   }
 
-  function createTodoItem(todo) {
-    if (todo.completed) {
-      todoList.insertAdjacentHTML(
-        "beforeend",
-        `<li id="${todo.id}" class="todo-item"><span class="completed">${todo.text}</span><i class="fas fa-trash"></i></li>`
-      );
-    } else {
-      todoList.insertAdjacentHTML(
-        "beforeend",
-        `<li id="${todo.id}" class="todo-item"><span>${todo.text}</span><i class="fas fa-trash"></i></li>`
-      );
-    }
-  }
-
-  function updateList(updatedTodos) {
-    todosArray = updatedTodos.map(todo => {
+  function updateList(updatedTodosArr) {
+    todosArray = updatedTodosArr.map(todo => {
       return { ...todo };
     });
-    if (!updatedTodos.length) {
-      window.localStorage.removeItem("todoList");
-    } else {
-      saveToStorage(updatedTodos);
-    }
+
+    !todosArray.length
+      ? window.localStorage.removeItem("todoList")
+      : saveToStorage(todosArray);
+
     renderTodos();
   }
 
-  function deleteTodo(id) {
-    updateList(todosArray.filter(todo => todo.id !== id));
-  }
+  // function deleteTodo(id) {
+  //   updateList(todosArray.filter(todo => todo.id !== id));
+  // }
 
-  function toggleCompleted(id) {
-    const todoIndex = locateTodoItem(id);
-    const toggleList = todosArray.map(todo => {
-      return { ...todo };
-    });
+  // function toggleCompleted(id) {
+  //   const todoIndex = locateTodoItem(id);
+  //   const toggleList = todosArray.map(todo => {
+  //     return { ...todo };
+  //   });
 
-    if (todoIndex >= 0) {
-      toggleList[todoIndex].completed = !toggleList[todoIndex].completed;
-    }
-    updateList(toggleList);
-  }
+  //   if (todoIndex >= 0) {
+  //     toggleList[todoIndex].completed = !toggleList[todoIndex].completed;
+  //   }
+  //   updateList(toggleList);
+  // }
 
   function saveToStorage(list) {
     window.localStorage.setItem("todoList", JSON.stringify(list));
@@ -128,6 +129,28 @@
   function locateTodoItem(id) {
     return todosArray.findIndex(todo => todo.id === id);
   }
-
-  init();
 })();
+
+// const hideCompleted = document.getElementById("hide-complete");
+// hideCompleted.addEventListener("click", () => {
+//   displayCompleted = !displayCompleted;
+//   renderTodos();
+// });
+
+// const deleteAllCompleted = document.getElementById("remove-complete");
+// deleteAllCompleted.addEventListener("click", () => {
+//   updateList(todosArray.filter(todo => !todo.completed));
+// });
+
+// function addListeners() {
+//   const todoList = document.getElementById("todo-list");
+//   todoList.addEventListener("click", addButtonClick);
+// }
+
+// function addButtonClick(e) {
+//   const element = e.srcElement.localName;
+//   const itemId = e.target.parentNode.id;
+
+//   if (element === "i") deleteTodo(itemId);
+//   if (element === "span") toggleCompleted(itemId);
+// }
